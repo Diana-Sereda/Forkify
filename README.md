bcbdac66cd3af3b52d4bc3976a84f1edb3591115
Diana-Sereda <diana_s29@mail.ru>
2022-05-28 14:52:51 +0700

    first init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forkify project
+
+Recipe app with custom recipe uploads created as practical project during the "The Complete JavaScript Course 2022: From Zero to Expert!" learning
---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3f670a3d-86e7-49e6-84b2-6b6ebe71bc99';
+export const MODAL_CLOSE_SEC = 2.5;
